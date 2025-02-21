84837ee097efcaa64a2989fcc78dc69f305e21cd
Brahimdiarra <brahimdiarra60@gmail.com>
2025-02-21 19:27:35 +0100

    ajoute de numero de quiz et le score et gestion d'affiche de quiz et rejoue

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-    afficherQuestion();
+    // Réinitialiser le quiz au chargement de la page
+    initialiserQuiz();
+
+    // Attacher l'événement au bouton "Valider" une seule fois
     document.getElementById("bouton-valider").addEventListener("click", questionSuivante);
 });
